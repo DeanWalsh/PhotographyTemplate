5c2cf9cc5d3b1abc755f74633663d1756fe49745
DeanWalsh <deanochambers@gmail.com>
2021-11-25 13:06:35 +0000

    Desktop responsive updates

--- src/components/HomeContact.vue
+++ src/components/HomeContact.vue
@@ -5,9 +5,9 @@
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum eos eius
                 distinctio beatae?
             </h3>
-            <img src="../assets/img/home/lukasz-szmigiel-2ShvY8Lf6l0-unsplash.jpg" alt="contact" width="900px"
+            <img class="contact-image" src="../assets/img/home/lukasz-szmigiel-2ShvY8Lf6l0-unsplash.jpg" alt="contact" width="900px"
                         height="500px" />
-            <img src="../assets/img/home/v2osk-IHKBF23A_iw-unsplash.jpg" alt="contact" width="900px"
+            <img class="contact-image" src="../assets/img/home/v2osk-IHKBF23A_iw-unsplash.jpg" alt="contact" width="900px"
                         height="500px" />
             <div class="contact-content">
                 <p class="description">
@@ -22,10 +22,3 @@
         </div>
     </div>
 </template>
-
-<style scoped>
-img {
-    width: 900px;
-    height: 500px;
-}
-</style>
